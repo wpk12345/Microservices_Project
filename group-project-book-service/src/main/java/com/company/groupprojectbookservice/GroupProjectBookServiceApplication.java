c48eedd4e4bd4ebc6112eb2b139b419c684f6dc5
Vijaya <vijayanagamani@gmail.com>
2019-07-27 14:19:19 -0400

    Adding book service

---
+++ group-project-book-service/src/main/java/com/company/groupprojectbookservice/GroupProjectBookServiceApplication.java
@@ -0,0 +1,13 @@
+package com.company.groupprojectbookservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroupProjectBookServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GroupProjectBookServiceApplication.class, args);
+	}
+
+}
