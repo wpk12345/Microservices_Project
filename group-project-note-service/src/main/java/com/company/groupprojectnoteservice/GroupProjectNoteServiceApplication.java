a024a30cd1ea2797c94758df7122bef9571955be
William King <wpk12345@gmail.com>
2019-07-28 19:58:47 -0400

    added note-service

---
+++ group-project-note-service/src/main/java/com/company/groupprojectnoteservice/GroupProjectNoteServiceApplication.java
@@ -0,0 +1,13 @@
+package com.company.groupprojectnoteservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroupProjectNoteServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GroupProjectNoteServiceApplication.class, args);
+	}
+
+}
